afb508df2b2478a0eb8517dc5acdc1222fc79b52
Yassine.L <y.labhih@gmail.com>
2022-12-16 23:15:27 +0100

    add config file

---
+++ app/core/Config.php
@@ -0,0 +1,25 @@
+<?php
+
+//website name
+define("WEBSITE_TITLE", 'Best shoes');
+
+//database name
+define('DB_NAME', "bestshoes_db");
+define('DB_USER', "root");
+define('DB_PASS', "");
+define('DB_TYPE', "mysql");
+define('DB_HOST', "localhost");
+
+
+define('THEME', 'bestshoes');
+
+
+//define debug mode
+define('DEBUG', true);
+
+if(DEBUG){
+    ini_set('display_errors', 1);
+}else{
+    ini_set('display_errors', 0);
+}
+
