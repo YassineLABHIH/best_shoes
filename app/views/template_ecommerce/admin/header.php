116c043b82fa05139add2f342f0f5c9de4d3cdb8
Yassine.L <y.labhih@gmail.com>
2022-12-18 18:17:05 +0100

    add nodes modules

--- app/views/template_ecommerce/admin/header.php
+++ app/views/template_ecommerce/admin/header.php
@@ -17,14 +17,16 @@
   <link href="https://fonts.gstatic.com" rel="preconnect">
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
 
-  <!-- Vendor CSS Files -->
-  <link href="<?= ASSETS . THEME ?>admin/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-  <link href="<?= ASSETS . THEME ?>admin/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
-  <link href="<?= ASSETS . THEME ?>admin/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
-  <link href="<?= ASSETS . THEME ?>admin/vendor/quill/quill.snow.css" rel="stylesheet">
-  <link href="<?= ASSETS . THEME ?>admin/vendor/quill/quill.bubble.css" rel="stylesheet">
-  <link href="<?= ASSETS . THEME ?>admin/vendor/remixicon/remixicon.css" rel="stylesheet">
-  <link href="<?= ASSETS . THEME ?>admin/vendor/simple-datatables/style.css" rel="stylesheet">
+<link rel="stylesheet" href="../../../../public/assets/node_modules/simple-datatables/src/style.css">
+  
+  <!-- Node CSS Files -->
+  <link href="<?= ASSETS . NODE ?>bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+  <link href="<?= ASSETS . NODE ?>bootstrap-icons/font/bootstrap-icons.css" rel="stylesheet">
+  <link href="<?= ASSETS . NODE ?>boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="<?= ASSETS . NODE ?>quill/dist/quill.snow.css" rel="stylesheet">
+  <link href="<?= ASSETS . NODE ?>quill/dist/quill.bubble.css" rel="stylesheet">
+  <link href="<?= ASSETS . NODE ?>remixicon/fonts/remixicon.css" rel="stylesheet">
+  <link href="<?= ASSETS . NODE ?>simple-datatables/src/style.css" rel="stylesheet">
 
   <!-- Template Main CSS File -->
   <link href="<?= ASSETS . THEME ?>admin/css/style.css" rel="stylesheet">
